2f7e49063531a908c78e299e7af3357c7209efe6
hphong <hp.hong@hotmail.com>
2022-02-21 14:16:53 +0900

    modified1

--- src/IvihoPPTPrimeHeader.js
+++ src/IvihoPPTPrimeHeader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { folderArray, linkArray, parentMenuArray } from "./folderArray";
 
 const IvihoPPTPrimeHeader = ({
